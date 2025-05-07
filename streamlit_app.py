4de52c38a1d2130f3ce3a2e9eb06c9793ba16af2
AntoITA <79995892+AntoITA@users.noreply.github.com>
2025-05-07 10:53:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,12 @@ with col2:
 totale_investimento_iniziale = prezzo_acquisto + spese_notarili + ristrutturazione + altre_spese
 
 # Web calcolo Spese acquisto casa
-
-<button onclick="window.open('https://www.mutuisupermarket.it/calcolo-mutuo/calcolo-spese-acquisto-casa', '_blank')">
-  Calcola Spese Acquisto Casa
-</button>
+st.markdown("""
+    <button onclick="window.open('https://www.mutuisupermarket.it/calcolo-mutuo/calcolo-spese-acquisto-casa', '_blank')"
+    style="padding: 0.5em 1em; font-size: 16px; border: none; background-color: #4CAF50; color: white; border-radius: 5px; cursor: pointer;">
+    Calcola Spese Acquisto Casa
+    </button>
+""", unsafe_allow_html=True)
 
 # Opzione di mutuo
 mutuo = st.checkbox("Considera mutuo nel calcolo", value=True)
